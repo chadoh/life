a5e3560bdbd55d13360c87661a4c1da02439d1b4
Chad Ostrowski <hi@chadoh.com>
2015-07-17 11:03:28 -0400

    Make adding dates work in not-Chrome
    When you add an event, the date is now selected with a radio button,
    instead of an `<input type="date">`. That means it works everywhere!
    
    I also refactored `WeekDetail` heavily to make it easier to reason
    about, splitting cohesive chunks of it into new components contained
    within the same file.

--- src/components/WeekDetail.jsx
+++ src/components/WeekDetail.jsx
@@ -8,16 +8,114 @@ import EventService from '../services/EventService';
 import { Range } from 'immutable';
 import { Link } from 'react-router';
 
-export default class WeekDetail extends React.Component {
-  constructor() {
-    super()
+class Event extends React.Component {
+  deleteEvent(e) {
+    let id = +e.target.getAttribute('data-id');
+    let weekno = +e.target.getAttribute('data-weekno');
+    EventService.destroy(this.props.slug, id, weekno)
+      .catch((err) => {
+        alert("There was an error destroying the event");
+        console.log("Error destroying event", err);
+      });
+  }
+
+  render() {
+    let deleteButton = !this.props.authed ? '' :
+      <td>
+        <button onClick={this.deleteEvent.bind(this)} className="pseudo" data-id={this.props.event.get('id')} data-weekno={this.props.weekno}>
+          &times;
+        </button>
+      </td>
+
+    return (
+      <tr>
+        <td>{Emoji.get(this.props.event.get('emoji'))}</td>
+        <td>{this.props.event.get('summary')}</td>
+        <td className="text-muted">{this.props.event.get('date')}</td>
+        {this.props.event.get('id') ? deleteButton : ''}
+      </tr>
+    )
+  }
+}
+
+class NewEventForm extends React.Component {
+  constructor(props) {
+    super(props)
     this.state = {
       summary: '',
       emoji: '',
       date: '',
-      events: null
-    };
-    this._onChange = this._onChange.bind(this);
+    }
+    this.selectDate = this.selectDate.bind(this)
+  }
+
+  addEvent(e) {
+    e.preventDefault();
+    EventService.create(this.props.slug, this.state.summary, this.state.emoji, this.state.date)
+      .then(() => {
+        this.setState({summary: '', emoji: ''});
+        this.refs.summary.getDOMNode().focus();
+      })
+      .catch((err) => {
+        alert("There was an error creating the event");
+        console.log("Error creating event", err);
+      });
+  }
+
+  get end() {
+    return UserStore.dateOf(+this.props.weekno + 1);;
+  }
+
+  selectDate(e) {
+    console.log(e.target.value);
+    this.setState({date: e.target.value})
+  }
+
+  get dates() {
+    let dates = []
+    let date = this.props.start;
+    while (date < this.end) {
+      dates.push(
+        <label key={date}>
+          <br/><input type="radio" name="date" value={date.toISOString().replace(/T.+/, '')} onChange={this.selectDate}/>
+          <span className="checkable">{date.toDateString()}</span>
+        </label>
+      )
+      date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)
+    }
+    return dates
+  }
+
+  render() {
+    return (
+      <form role="form" onSubmit={this.addEvent.bind(this)}>
+        <h2>Add a new event</h2>
+        <p>
+          <label htmlFor="summary">Summary</label>
+          <input required id="summary" name="summary" className="form-control" type="text" placeholder="Left for Mars" valueLink={this.linkState('summary')} ref="summary"/>
+        </p>
+        <p>
+          <label htmlFor="emoji">Emoji</label>
+          <input required id="emoji" name="emoji" className="form-control" type="text" placeholder="milky_way" valueLink={this.linkState('emoji')} />
+          <small className="text-muted">Use the names from <a target="_blank" href="http://www.emoji-cheat-sheet.com/">the emoji cheat sheet</a></small>
+        </p>
+        <p>
+          <label htmlFor="date">Date</label>
+          {this.dates}
+        </p>
+        <button type="submit" className="btn btn-default">Save</button>
+      </form>
+    )
+  }
+}
+
+ReactMixin(NewEventForm.prototype, React.addons.LinkedStateMixin);
+
+export default class WeekDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { events: null }
+    this._onChange = this._onChange.bind(this)
   }
 
   componentWillMount() {
@@ -42,39 +140,18 @@ export default class WeekDetail extends React.Component {
   }
 
   render() {
-    let deleteButton = !this.authed ? '' :
-      <td><button onClick={this.deleteEvent.bind(this)} className="pseudo">&times;</button></td>
-
     let events = this.state.events && this.state.events.map(event => (
-      <tr key={event.get('id')} data-id={event.get('id')} data-weekno={this.props.params.weekno}>
-        <td>{Emoji.get(event.get('emoji'))}</td>
-        <td>{event.get('summary')}</td>
-        <td className="text-muted">{event.get('date')}</td>
-        {event.get('id') ? deleteButton : ''}
-      </tr>
+      <Event
+        key={event.get('date') + event.get('id')}
+        slug={this.props.params.slug}
+        weekno={this.props.params.weekno}
+        event={event}
+        authed={this.authed}
+      />
     ));
 
     let form = !this.authed ? '' :
-      <form role="form" onSubmit={this.addEvent.bind(this)}>
-        <h2>Add a new event</h2>
-        <p>
-          <label htmlFor="summary">Summary</label>
-          <input required id="summary" name="summary" className="form-control" type="text" placeholder="Left for Mars" valueLink={this.linkState('summary')} ref="summary"/>
-        </p>
-        <p>
-          <label htmlFor="emoji">Emoji</label>
-          <input required id="emoji" name="emoji" className="form-control" type="text" placeholder="milky_way" valueLink={this.linkState('emoji')} />
-          <small className="text-muted">Use the names from <a target="_blank" href="http://www.emoji-cheat-sheet.com/">the emoji cheat sheet</a></small>
-        </p>
-        <p>
-          <label htmlFor="date">Date</label>
-          <input required id="date" name="date" className="form-control"
-                 type="date" min={this.start.toISOString().replace(/T.+/, '')}
-                 max={this.end.toISOString().replace(/T.+/, '')}
-                 valueLink={this.linkState('date')} />
-        </p>
-        <button type="submit" className="btn btn-default">Save</button>
-      </form>
+      <NewEventForm weekno={this.props.params.weekno} start={this.start} slug={this.props.params.slug} />
 
     return (
       <div className="week-detail-wrap">
@@ -82,7 +159,7 @@ export default class WeekDetail extends React.Component {
         {!UserStore.getState().user.get('born') ? '' :
           <aside className="week-detail">
             <h1 className="brand">
-              Week of {this.start.toDateString()} <br />
+              Week of {this.start.toDateString()}<br/>
               <small>{Math.floor(+this.props.params.weekno/52)} years old</small>
             </h1>
             <table>
@@ -95,37 +172,7 @@ export default class WeekDetail extends React.Component {
     )
   }
 
-  deleteEvent(e) {
-    let row = e.target.closest('tr');
-    let id = +row.getAttribute('data-id');
-    let weekno = +row.getAttribute('data-weekno');
-    EventService.destroy(this.props.params.slug, id, weekno)
-      .catch((err) => {
-        alert("There was an error destroying the event");
-        console.log("Error destroying event", err);
-      });
-  }
-
-  addEvent(e) {
-    e.preventDefault();
-    EventService.create(this.props.params.slug, this.state.summary, this.state.emoji, this.state.date)
-      .then(() => {
-        this.setState({summary: '', emoji: '', date: ''});
-        this.refs.summary.getDOMNode().focus();
-      })
-      .catch((err) => {
-        alert("There was an error creating the event");
-        console.log("Error creating event", err);
-      });
-  }
   get start() {
     return UserStore.dateOf(+this.props.params.weekno);
   }
-
-  get end() {
-    let startOfNextWeek = UserStore.dateOf(+this.props.params.weekno + 1);
-    return new Date(startOfNextWeek.getFullYear(), startOfNextWeek.getMonth(), startOfNextWeek.getDate() - 1);
-  }
 }
-
-ReactMixin(WeekDetail.prototype, React.addons.LinkedStateMixin);

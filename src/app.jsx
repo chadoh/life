0e1c87f69ebc4b77c4517ada682af1cd902ad287
Chad Ostrowski <hi@chadoh.com>
2015-08-15 16:07:41 -0400

    Add Pricing page

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import Router, {Route} from 'react-router';
 import AuthenticatedApp from './components/AuthenticatedApp'
 import Login from './components/Login';
 import Home from './components/Home';
+import Pricing from './components/Pricing';
 import User from './components/User';
 import WeekDetail from './components/WeekDetail';
 import UserEdit from './components/UserEdit';
@@ -13,6 +14,8 @@ var routes = (
   <Route handler={AuthenticatedApp}>
     <Route name="login" handler={Login}/>
     <Route name="home" path="/" handler={Home}/>
+    <Route name="pricing" path="/pricing" handler={Pricing}/>
+
     <Route name="account" path="/account" handler={UserEdit} />
     <Route name="user" path="/:slug" handler={User} ignoreScrollBehavior={true}>
       <Route name="week" path="week/:weekno" handler={WeekDetail} ignoreScrollBehavior={true}/>
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Pricing extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="bg-light full-height">
+          <div className="container">
+            <h1 className="brand">Choose your payment plan <sup><a href="#addendum">*</a></sup></h1>
+            <div className="payment-options">
+              <a className="payment-option">
+                <h2 className="brand">
+                  <div className="vertical-centering">
+                    <div>
+                      Annual
+                    </div>
+                  </div>
+                </h2>
+                <div className="price">
+                  <sup className="currency-marker">$</sup>
+                  <span className="amount">10</span>
+                  <div className="interval">each year</div>
+                </div>
+              </a>
+              <a className="payment-option">
+                <h2 className="brand">
+                  <div className="vertical-centering">
+                    <div>
+                      Lifetime
+                    </div>
+                  </div>
+                </h2>
+                <div className="price">
+                  <sup className="currency-marker">$</sup>
+                  <span className="amount">50</span>
+                  <div className="interval">one-time fee</div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-dark full-height">
+          <div className="container">
+            <h1 className="brand" id="addendum">Ads suck !</h1>
+            <p>
+              Entire.Life will never advertise or sell your information to third parties.
+              Instead, we&lsquo;re asking you to pay what we think is a reasonable amount for our service.
+            </p>
+            <p>
+              If the amounts we&lsquo;re charging seem too high, please&nbsp;
+              <a href="mailto:hi+entire.life@chadoh.com">email us</a> to see if we
+              can work out a deal.
+            </p>
+            <p>
+              If you&lsquo;re the tech-savvy type, you can also&nbsp;
+              <a href="https://github.com/chadoh/life">host your own!</a>&nbsp;
+              Entire.Life is open source software, which means that you can read
+              all of the code that makes it, as well as modify it and run it on
+              your own servers. <strong>Note:</strong> if you choose to host
+              your own, you will not have the super-cool domain name
+              "entire.life", and you will miss out on future social features.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}

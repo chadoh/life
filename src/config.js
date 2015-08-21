9faeceade7153a7b6d12028a10a903d81bda54fa
Chad Ostrowski <hi@chadoh.com>
2015-08-21 02:26:27 -0400

    Use SSL heroku endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // API_URL: 'http://localhost:5000/',
-  API_URL: 'http://entire-life.herokuapp.com/',
+  API_URL: 'https://entire-life.herokuapp.com/',
 
   // STRIPE_PUBLISHABLE_KEY: 'pk_4QXgq0EuigTEkoZF72kdekSBUF6gs', // test
   STRIPE_PUBLISHABLE_KEY: 'pk_n6D1XV27lIqhZyg1sjLeE0HjyPo7X', // live

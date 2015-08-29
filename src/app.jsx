3dc25bd08bdf57f4fb1d4718c6162258b44b5087
Chad Ostrowski <hi@chadoh.com>
2015-08-29 14:11:37 -0400

    Add payment test page to facilitate StackOverflow discussion

--- src/app.jsx
+++ src/app.jsx
@@ -9,6 +9,7 @@ import Signup from './components/Signup'
 import User from './components/User'
 import WeekDetail from './components/WeekDetail'
 import UserEdit from './components/UserEdit'
+import PaymentFormTest from './components/PaymentFormTest'
 import RouterContainer from './services/RouterContainer'
 import LoginActions from './actions/LoginActions'
 import jQuery from 'jquery'
@@ -23,6 +24,8 @@ var routes = (
 
     <Route name="signup" path="signup" handler={Signup}/>
     <Route name="confirm" path="users/confirmation" handler={Confirm}/>
+
+    <Route name="payment-form-test" path="payment-form-test" handler={PaymentFormTest}/>card_number
 
     <Route name="account" path="/account" handler={UserEdit} />
     <Route name="user" path="/:slug" handler={User} ignoreScrollBehavior={true}>

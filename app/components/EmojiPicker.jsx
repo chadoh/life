62282d94fdc46dccdb67a98ea0bbbea9930be472
Chad Ostrowski <hi@chadoh.com>
2015-11-26 12:36:58 -0500

    Show which emoji is being hovered
    And fix searching for special characters

--- app/components/EmojiPicker.jsx
+++ app/components/EmojiPicker.jsx
@@ -3,30 +3,57 @@ import ReactMixin from 'react-mixin'
 import ReactEmoji from 'react-emoji'
 import emojiMap from 'emoji-annotation-to-unicode'
 
+const emojis = query => {
+  query = query.replace(/:/g, '').replace(/([\+\-])/g, "\\$&")
+
+  return Object.keys(emojiMap).
+    filter(key => key.match(`^(?:${query})`)).
+    slice(0,68)
+}
+
 export default class EmojiPicker extends React.Component {
-  emojis() {
-    let query = this.props.query.replace(/:/g, '')
-    if(query && query[0].match(/[\+\-]/)) {
-      query = `${'\\'}${query}`
+  constructor(props) {
+    super(props)
+    this.state = {
+      hovered: null
     }
-    return Object.keys(emojiMap).
-      filter(key => key.match(`^(?:${query})`)).
-      slice(0,68)
+
+    this.blurred = this.blurred.bind(this)
+  }
+
+  footer() {
+    if(this.state.hovered) {
+      return <span><br/>{this.state.hovered}</span>
+    }
+  }
+
+  hovered(emoji) {
+    this.setState({hovered: emoji})
+  }
+
+  blurred() {
+    this.setState({hovered: null})
   }
 
   render() {
+    let emojiLinks = emojis(this.props.query).map(emoji => {
+      const yay = `:${emoji}:`
+      return (
+        <a key={emoji} className="emoji-picker-emoji"
+          onClick={this.props.onSelect.bind(null, yay)}
+          onMouseEnter={this.hovered.bind(this, yay)}
+          onMouseLeave={this.blurred}
+          style={{padding: '0.3rem', cursor: 'pointer'}}>
+          {this.emojify(yay, {singleEmoji: true})}
+        </a>
+      )
+    })
+    if(emojiLinks.length === 0) emojiLinks = "No emojis found";
+
     return (
       <span style={this.props.style}>
-        {this.emojis().map(emoji => {
-          const yay = `:${emoji}:`
-          return (
-            <a key={emoji} className="emoji-picker-emoji"
-              onClick={this.props.onSelect.bind(null, yay)}
-              style={{padding: '0.2rem', cursor: 'pointer'}}>
-              {this.emojify(yay, {singleEmoji: true})}
-            </a>
-          )
-        })}
+        {emojiLinks}
+        {this.footer()}
       </span>
     )
   }

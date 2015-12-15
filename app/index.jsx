6b41125067bb8727a1097c27ab018b88ff2f4246
Chad Ostrowski <hi@chadoh.com>
2015-12-15 13:42:20 -0500

    Stub out Contact page

--- app/index.jsx
+++ app/index.jsx
@@ -9,6 +9,7 @@ import ReactDOM from 'react-dom'
 import {Router, Route, IndexRoute} from 'react-router'
 import Home from './components/Home'
 import Team from './components/Team'
+import Contact from './components/Contact'
 import Pricing from './components/Pricing'
 import Signin from './components/Signin'
 import SigningUp from './components/SigningUp'
@@ -31,6 +32,7 @@ let router = (
   <Router history={history}>
     <Route path="/" component={Home}/>
     <Route path="team" component={Team}/>
+    <Route path="contact" component={Contact}/>
     <Route path="signin" component={Signin}/>
 
     <Route path="pricing" component={Pricing}/>
